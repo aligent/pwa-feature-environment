0f73d5f9327b34fa0f45383f7b60e8cd30c1d185
Krishan Thisera <krishan.thisera@aligent.com.au>
2022-07-04 13:24:29 +0930

    DO-1127: Feature Deployment Initial commit

---
+++ lib/fe-feature-deployment.ts
@@ -0,0 +1,21 @@
+import { Stack, StackProps, aws_s3_deployment as s3Deploy, aws_s3 as s3, Fn, CfnOutput} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+interface FEFeatureDeploymentProps extends StackProps {
+    readonly bucket: string
+    readonly assets: string
+  }
+  
+  export class FEFeatureDeployment extends Stack {
+    constructor(scope: Construct, id: string, props: FEFeatureDeploymentProps){
+      super(scope, id, props);
+      
+      new s3Deploy.BucketDeployment(this, 'Deployment', {
+        sources: [s3Deploy.Source.asset(props.assets)],
+        prune: false,
+        destinationBucket: s3.Bucket.fromBucketName(this,
+            'import-bucket',
+            props.bucket)
+      });
+    }
+  }
--- lib/fe-feature-env-stack.ts
+++ lib/fe-feature-env-stack.ts
@@ -1,16 +1,36 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_s3_deployment as s3Deploy, aws_s3 as s3, Fn} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { StaticHosting, StaticHostingProps } from '@aligent/cdk-static-hosting';
+import { Behavior } from 'aws-cdk-lib/aws-cloudfront';
+import { assert } from 'console';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class FeFeatureEnvStack extends Stack {
+const HostingStackProps : StaticHostingProps = {
+  subDomainName: 'fetest',
+  domainName: 'bizkt.com.au',
+  certificateArn: 'arn:aws:acm:us-east-1:166381158005:certificate/64b49688-5db3-4665-aa62-45673ff356eb',
+  createDnsRecord: false,
+  createPublisherGroup: true,
+  createPublisherUser: true,
+  enableErrorConfig: true
+};
+export class FEFeatureEnvironment extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'FeFeatureEnvQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    /*
+      Static Hosting Resources
+    */
+    let behaviors: Behavior[] = [];
+      behaviors.push(
+        {
+          pathPattern: `/*`,
+          isDefaultBehavior: true,
+        }
+      )
+    
+   new StaticHosting(this, 'hosting-stack', {...HostingStackProps, behaviors: behaviors });
   }
 }
